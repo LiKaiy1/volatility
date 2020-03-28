54cef697e87617e4614b80d43daf560f40e859eb
LiKaiy1 <kl2538@nyu.edu>
2020-03-28 16:40:33 +0800

    Coding Finished. Paper Started.

--- project/project_r_patch.R
+++
@@ -1,7 +0,0 @@
-library(rugarch)
-
-garchSpec <- ugarchspec(
-  variance.model=list(model="sGARCH", garchOrder=c(1,1)),
-  mean.model=list(armaOrder = c(0,0),include.mean=F))
-fit = ugarchfit(spec = garchSpec, data = simulation_result$r)
-print(fit)
